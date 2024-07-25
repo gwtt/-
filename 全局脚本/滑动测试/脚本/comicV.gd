2c6bcf639b3dcf7d3a112dbd69386f1f8ffebe4a
da11ybreak <3171632055@qq.com>
2024-07-25 18:08:57 +0800

    滑动测试
    添加了图片滑动功能测试，comicH是水平场景，comicV是垂直场景

---
+++ 全局脚本/滑动测试/脚本/comicV.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@onready var camera_2d = $Camera2D
+
+var isDrag = false
+var StartPos = 0
+var StartCamPos = 0
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == 1:
+			if event.is_pressed():
+				isDrag = true
+				StartPos = event.position.y
+				StartCamPos = camera_2d.position.y
+			else:
+				isDrag = false
+				StartPos = 0
+	if 	isDrag:
+		var OFFSET = StartPos - event.position.y
+		camera_2d.position.y = StartCamPos + OFFSET
+
+	
+	pass
