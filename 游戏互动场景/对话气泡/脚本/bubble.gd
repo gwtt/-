4d0d13699d927a30d2a1f8f4e5db9860426d82d4
da11ybreak <3171632055@qq.com>
2024-07-28 23:03:07 +0800

    气泡功能完善

--- 游戏互动场景/对话气泡/脚本/bubble.gd
+++ 游戏互动场景/对话气泡/脚本/bubble.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var button = $Button
 @onready var boom = $Button/Boom
+@onready var timer = $Button/Timer
+
 
 var StartPos : Vector2
 var EndPos : Vector2
@@ -32,10 +34,20 @@ func _button_appear():
 	
 
 func _on_button_pressed():
+	button.set_disabled(true)
 	EndMod = Color(1,1,1,0)
 	var DisPos = button.position+Vector2(0,500)
-	#位置消失
-	#create_tween().tween_property(button,"position",DisPos,1.5)
 	#逐渐透明
-	create_tween().tween_property(button,"modulate",EndMod,1.5)
+	create_tween().tween_property(button,"modulate",EndMod,1)
 	boom.set_visible(true)
+	await get_tree().create_timer(1).timeout
+	button.queue_free()
+	var parent = get_node("..")
+	parent._progress_change()
+
+func _on_timer_timeout():
+	var DisPos = button.position-Vector2(0,500)
+	#飞出屏幕
+	create_tween().tween_property(button,"position",DisPos,2)
+	await get_tree().create_timer(2).timeout
+	button.queue_free()
--- 游戏互动场景/对话气泡/脚本/dialog.gd
+++ 游戏互动场景/对话气泡/脚本/dialog.gd
@@ -1,16 +1,28 @@
 extends Node2D
 
-@onready var bubble_1 = $CharacterBody2D/Bubble1
-@onready var bubble_2 = $CharacterBody2D/Bubble2
+@onready var character = $CharacterBody2D/AnimatedSprite2D
+@onready var check_button = $CheckButton
+@onready var progressbar = $TextureProgressBar
+
 var bubble_scene = preload("res://游戏互动场景/对话气泡/场景/bubble.tscn")
 var bubble_init_array : Array
+var isAlive = true
 
 func _ready():
 	for i in range(30):
 		var bubble_init = bubble_scene.instantiate()
 		bubble_init_array.push_front(bubble_init)
+
+func _on_check_button_pressed():
+	check_button.set_disabled(true)
 	for i in range(30):
 		add_child(bubble_init_array[i])
 		bubble_init_array[i].set_position(Vector2(randi_range(-700,700),randi_range(200,400)))
 		bubble_init_array[i]._button_appear()
 		await get_tree().create_timer(2).timeout
+
+func _progress_change():
+	create_tween().tween_property(progressbar,"value",progressbar.value+0.2,0.5)
+	if (progressbar.value == 1 and isAlive):
+		character.play("dead")
+		isAlive = false
