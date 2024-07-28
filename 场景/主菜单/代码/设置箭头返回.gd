518f59fb842970509be3630e3897c6b0f429b5aa
Gwtt <1213605030@qq.com>
2024-07-28 18:16:52 +0800

    添加关于功能

--- 场景/主菜单/代码/设置箭头返回.gd
+++ 场景/主菜单/代码/设置箭头返回.gd
@@ -1,17 +1,19 @@
 extends TextureButton
-
-@onready var start_panel = $"../../../../../初始界面"
-@onready var setting_panel = $"../../../.."
+#开始界面
+@export var start_panel:Control
+#自己在的panel
+@export var own_panel:Control
 @export var change_position:Vector2 = Vector2(0,5)
 var origin_position
 func _ready():
-	setting_panel.modulate = Color(1,1,1,0)
-	setting_panel.mouse_filter = MOUSE_FILTER_IGNORE
+	own_panel.visible = false
+	own_panel.modulate = Color(1,1,1,0)
 	connect("button_up",back_to_main_menu)
 	connect("button_down",on_button_down)
 	connect("mouse_entered", _on_button_mouse_enter)
 
 func _on_button_mouse_enter():
+	print("hello")
 	if !origin_position:
 		origin_position = self.position
 func on_button_down():
@@ -19,9 +21,9 @@ func on_button_down():
 	tween.tween_property(self,"position",origin_position + change_position,0.2)
 	tween.tween_property(self,"modulate",Color(1,1,1,0.5),0.2)
 func back_to_main_menu():
-	setting_panel.visible = false
-	start_panel.visible = true
 	var tween = create_tween().set_parallel(true).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(setting_panel,"modulate",Color(1,1,1,0),0.2)
+	tween.tween_property(own_panel,"modulate",Color(1,1,1,0),0.2)
 	tween.tween_property(start_panel,"modulate",Color(1,1,1,1),0.2)
+	tween.tween_property(own_panel,"visible",false,0.2)
+	tween.tween_property(start_panel,"visible",true,0.2)
 	tween.tween_property(self,"modulate",Color(1,1,1,1),0.2)
