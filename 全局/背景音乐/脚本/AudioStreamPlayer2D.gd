86a96369a28b164ff1f7c53e1bbe38283b8c974e
Gwtt <1213605030@qq.com>
2024-07-28 20:01:30 +0800

    添加音乐

---
+++ 全局/背景音乐/脚本/AudioStreamPlayer2D.gd
@@ -0,0 +1,24 @@
+extends AudioStreamPlayer2D
+var screen_resolution_dictionary = {
+	0: -50,
+	1: -20,
+	2: -15,
+	3: -10,
+	4: -5,
+	5: 0,
+	6: 2,
+	7: 4,
+	8: 6,
+	9: 8,
+	10: 10
+}
+
+func _ready():
+	BaseSetting.music_change.connect(change_background_music_volume)
+	change_background_music_volume(BaseSetting.music)
+
+
+func change_background_music_volume(index):
+	var bus_index = AudioServer.get_bus_index("Master")
+	var current_volume = AudioServer.get_bus_volume_db(bus_index)
+	AudioServer.set_bus_volume_db(bus_index, screen_resolution_dictionary.get(index)) 
