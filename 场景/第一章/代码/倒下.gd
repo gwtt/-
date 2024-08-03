3e4ab6f5259bba88211e1da185f729eb29d61422
Gwtt <1213605030@qq.com>
2024-08-03 21:15:09 +0800

    添加倒下

---
+++ 场景/第一章/代码/倒下.gd
@@ -0,0 +1,11 @@
+extends GameInit
+
+@onready var 主角 = $"白色框/主角"
+
+var flag = false
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.is_pressed() and !flag:
+			主角.play("倒下")
+			flag = true
