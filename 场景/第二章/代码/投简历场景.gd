449671f0e8b8d14e6aa4f66b20664110030e7124
da11ybreak <3171632055@qq.com>
2024-08-18 22:08:12 +0800

    遮罩

--- 场景/第二章/代码/投简历场景.gd
+++ 场景/第二章/代码/投简历场景.gd
@@ -1,50 +1,20 @@
 extends Node2D
 
 var move_time:float = 1.0
-var is_out = false
 
 @onready var origin: Node2D = $"初始位置"
 @onready var display: Node2D = $"显示位置"
-@onready var content: Node2D = $"投递内容"
-@onready var disappear_pass: Node2D = $"消失位置_跳过"
-@onready var disappear_post: Node2D = $"消失位置_投递"
+@onready var content: Node2D = $"Mask/投递内容"
+@onready var disappear_pass: Node2D = $"消失位置_投递"
+@onready var disappear_post: Node2D = $"消失位置_跳过"
+@onready var mask: Sprite2D = $Mask
 
 var content_init
 const content_scene = preload("res://场景/第二章/场景/投递内容.tscn")
 
-func _on_左边按钮_pressed() -> void:
-	if !is_out:
-		var tween = create_tween().set_ease(Tween.EASE_OUT)
-		tween.tween_property(content,"global_position",disappear_pass.global_position,move_time)
-		await get_tree().create_timer(1).timeout
-		content.queue_free()
-		is_out = true
-		create_node()
-	else:
-		var tween = create_tween().set_ease(Tween.EASE_OUT)
-		tween.tween_property(content_init,"global_position",disappear_pass.global_position,move_time)
-		await get_tree().create_timer(1).timeout
-		content_init.queue_free()
-		create_node()
-
-func _on_右边按钮_pressed() -> void:
-	if !is_out:
-		var tween = create_tween().set_ease(Tween.EASE_OUT)
-		tween.tween_property(content,"global_position",disappear_post.global_position,move_time)
-		await get_tree().create_timer(1).timeout
-		content.queue_free()
-		is_out = true
-		create_node()
-	else:
-		var tween = create_tween().set_ease(Tween.EASE_OUT)
-		tween.tween_property(content_init,"global_position",disappear_post.global_position,move_time)
-		await get_tree().create_timer(1).timeout
-		content_init.queue_free()
-		create_node()
-
 func create_node():
 	content_init = content_scene.instantiate()
-	add_child(content_init)
+	mask.add_child(content_init)
 	content_init.random_content()
 	content_init.global_position = origin.global_position
 	var tween = create_tween().set_ease(Tween.EASE_OUT)
--- 场景/第二章/代码/投递内容.gd
+++ 场景/第二章/代码/投递内容.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var salary: Label = $"工资"
 @onready var graduation: Label = $"学历"
 
+var move_time:float = 1.0
+
 var post_dictionary = {
 	1: "■■■",
 	2: "■■",
@@ -28,3 +30,19 @@ func random_content():
 	post.set_text(post_dictionary[randi_range(1,3)])
 	salary.set_text(salary_dictionary[randi_range(1,6)])
 	graduation.set_text(graduation_dictionary[randi_range(1,4)])
+
+func _on_左边按钮_pressed() -> void:
+	var tween = create_tween().set_ease(Tween.EASE_OUT)
+	var parent = get_node("../../")
+	tween.tween_property(self,"global_position",parent.disappear_pass.global_position,move_time)
+	await get_tree().create_timer(1).timeout
+	self.queue_free()
+	parent.create_node()
+
+func _on_右边按钮_pressed() -> void:
+	var tween = create_tween().set_ease(Tween.EASE_OUT)
+	var parent = get_node("../../")
+	tween.tween_property(self,"global_position",parent.disappear_post.global_position,move_time)
+	await get_tree().create_timer(1).timeout
+	self.queue_free()
+	parent.create_node()
