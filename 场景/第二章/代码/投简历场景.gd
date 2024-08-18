761f5bd33cc05a3ba81dd34269cc3da7dd0d7ddb
da11ybreak <3171632055@qq.com>
2024-08-18 22:13:42 +0800

    完成遮罩

--- 场景/第二章/代码/投简历场景.gd
+++ 场景/第二章/代码/投简历场景.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
-var move_time:float = 1.0
+var move_time:float = 1
+var right_scale = Vector2(0.236,-0.161)
+var count = 0
 
 @onready var origin: Node2D = $"初始位置"
 @onready var display: Node2D = $"显示位置"
@@ -15,6 +17,7 @@ const content_scene = preload("res://场景/第二章/场景/投递内容.tscn")
 func create_node():
 	content_init = content_scene.instantiate()
 	mask.add_child(content_init)
+	content_init.set_scale(right_scale)
 	content_init.random_content()
 	content_init.global_position = origin.global_position
 	var tween = create_tween().set_ease(Tween.EASE_OUT)
